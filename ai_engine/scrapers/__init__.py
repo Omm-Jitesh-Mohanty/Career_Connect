44a5fddbeeb1a15ba6f18bfb062a62cd775e88e0
Omm Jitesh Mohanty <ommjiteshmohanty@gmail.com>
2026-01-28 11:04:46 +0530

    Delete ai_engine directory

--- ai_engine/scrapers/__init__.py
+++
@@ -1,6 +0,0 @@
-# ai_engine/scrapers/__init__.py
-from .internship_scraper import InternshipScraper
-from .course_scraper import CourseScraper
-from .link_manager import LinkManager
-
-__all__ = ['InternshipScraper', 'CourseScraper', 'LinkManager']
